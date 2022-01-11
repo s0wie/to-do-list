a4b3701c9c1157d5c08c79e30ab7dc7d7dbdcd11
s0wie <sowieh@gmail.com>
2022-01-11 19:24:14 +0100

    Revert "last changes to layout in lists"
    This reverts commit 54d2183350990ab3ec4477bae08d6ba9b234285f.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,13 +3,10 @@
 // Variables
 const descriptions = document.querySelectorAll('.task-description');
 const showDetails = document.querySelector('.show-details'); // The button that says "show details"
-const createNoteForm = document.querySelector('.create-list-form');
-const createNoteButton = document.querySelector('.create-list-button');
 
 // Eventlisteners
 showDetails.addEventListener('click', toggleHidden);
 showDetails.addEventListener('click', changeText);
-createNoteButton.addEventListener('click', showNoteForm);
 
 // Functions
 function toggleHidden() {
@@ -24,10 +21,6 @@ function changeText() {
   } else {
     showDetails.textContent = 'show details';
   }
-}
-
-function showNoteForm() {
-  createNoteForm.classList.toggle('hidden');
 }
 
 // ADD TASK BY DOUBLE CLICKING CARD DOES NOT WORK EITHER
