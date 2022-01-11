150198fc0bff50326cd6a8d6f33bd2d0fa19204b
s0wie <sowieh@gmail.com>
2022-01-11 16:56:41 +0100

    show details button changes text on click

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,26 @@
 // TOGGLE SHOWING DETAILS
+
+// Variables
 const descriptions = document.querySelectorAll('.task-description');
-const details = document.querySelector('.show-details'); // The button that says "show details"
+const showDetails = document.querySelector('.show-details'); // The button that says "show details"
 
-details.addEventListener('click', toggleHidden);
+// Eventlisteners
+showDetails.addEventListener('click', toggleHidden);
+showDetails.addEventListener('click', changeText);
 
+// Functions
 function toggleHidden() {
   descriptions.forEach((description) => {
     description.classList.toggle('hidden');
   });
+}
+
+function changeText() {
+  if (showDetails.textContent === 'show details') {
+    showDetails.textContent = 'hide details';
+  } else {
+    showDetails.textContent = 'show details';
+  }
 }
 
 // ADD TASK BY DOUBLE CLICKING CARD DOES NOT WORK EITHER
