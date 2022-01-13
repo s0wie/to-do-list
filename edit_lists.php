3b7a4a5f266e67c6d99cccf5415602070b6cbcf5
s0wie <sowieh@gmail.com>
2022-01-13 14:23:09 +0100

    Deleted strict types on html pages

--- edit_lists.php
+++ edit_lists.php
@@ -1,7 +1,3 @@
-<?php
-
-declare(strict_types=1); ?>
-
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
 <?php require __DIR__ . '/app/tasks/fetch.php'; ?>
